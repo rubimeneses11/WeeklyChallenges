3c2d3571f9fdafc4e89db188907c8e0ccf97c060
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-09 17:23:21 -0500

    week 3 done

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,53 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
-        public bool ArrayContainsAFalse(bool[] vals)
+        public bool ArrayContainsAFalse(bool[] vals) 
         {
-            throw new NotImplementedException();
+            foreach (var item in vals)
+            {
+                if (item == false)
+                {
+                    return true;
+                }
+            }
+                return false;
         }
 
+       
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+            if (numbers.Sum() % 2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
-        public bool PasswordContainsUpperLowerAndNumber(string password)
+        
+        public bool PasswordContainsUpperLowerAndNumber(string password)  
         {
-            throw new NotImplementedException();
+            return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsNumber);
+
         }
 
-        public char GetFirstLetterOfString(string val)
+        public char GetFirstLetterOfString(string val) 
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
-        public char GetLastLetterOfString(string val)
+        public char GetLastLetterOfString(string val) 
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
-        public decimal Divide(decimal dividend, decimal divisor)
+       
+        public decimal Divide(decimal dividend, decimal divisor) 
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length-1] - nums[0];
         }
 
-        public int[] GetOddsBelow100()
+        public int[] GetOddsBelow100() 
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            for (int i = 1; i < 100; i+=2)
+            {
+                list.Add(i);
+            }
+            return list.ToArray();
         }
 
-        public void ChangeAllElementsToUppercase(string[] words)
+        public void ChangeAllElementsToUppercase(string[] words) 
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
